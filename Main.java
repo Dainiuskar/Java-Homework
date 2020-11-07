0acfc3d1a2fb29e085f539050e76d35f5902ade3
Dainius Karalevicius <dainius.karalevicus@gmail.com>
2020-11-07 17:54:51 +0200

    Change directories

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-// comment for new commit...
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.calculate("5x1+2-4!"));
-
-    }
-}
