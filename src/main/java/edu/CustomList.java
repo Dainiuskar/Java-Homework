82832fdf772b42813c2c58b53793238b716f07cf
Dainius Karalevicius <dainius.karalevicus@gmail.com>
2020-11-14 11:46:49 +0200

    Added new methos add()  and  removeAt

--- src/main/java/edu/CustomList.java
+++ src/main/java/edu/CustomList.java
@@ -20,4 +20,27 @@ public class CustomList {
     public int[] getAll() {
         return ints;
     }
+
+    public void add(int param){
+
+        int[] newInts = new int[this.ints.length+1];
+        for (int i=0; i<ints.length; i++){
+            newInts[i] = this.ints[i];
+        }
+        newInts[-1] = param;
+        this.ints = newInts;
+    }
+
+    public void removeAt(int position){
+
+        int[] newInts = new int[this.ints.length-1];
+        for (int i=0; i<ints.length; i++) {
+            if (i != position){
+                newInts[i] = this.ints[i];
+            }
+        }
+        this.ints = newInts;
+    }
+
+
 }
